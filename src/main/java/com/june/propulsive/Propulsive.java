182bbc743f1c2a0ea0b54dfbd79594690e6ad497
JuneDeveloper <JuneDeveloper@github.com>
2023-07-28 18:48:52 +0100

    Added Mercury, Venus and Mars

--- src/main/java/com/june/propulsive/Propulsive.java
+++ src/main/java/com/june/propulsive/Propulsive.java
@@ -18,36 +18,115 @@ public class Propulsive implements ModInitializer {
 
     @Override
     public void onInitialize() {
+        MERCURY.render();
+        VENUS.render();
         EARTH.render();
         MOON.render();
+        MARS.render();
+
         MapScreenKeybindRegister();
-        TickablePlanets.add(MOON);
+
+        TickablePlanets.add(MERCURY);
+        TickablePlanets.add(VENUS);
         TickablePlanets.add(EARTH);
+        TickablePlanets.add(MOON);
+        TickablePlanets.add(MARS);
     }
 
     // Dimensions
     public static RegistryKey<World> SPACE = RegistryKey.of(RegistryKeys.WORLD, new Identifier("propulsive:space"));
 
-    // Planet configuration
+    // Actually creating the planets
+    // Mercury
+    public static final double MERCURY_SPACE_POSX = 0.0;
+    public static final double MERCURY_SPACE_POSY = 0.0;
+    public static final double MERCURY_SPACE_POSZ = 0.0;
+    public static final float MERCURY_SPACE_SIZE = 0.5f;
+    public static final double MERCURY_HEIGHT = 128.0;
+    // Venus
+    public static final double VENUS_SPACE_POSX = 0.0;
+    public static final double VENUS_SPACE_POSY = 6.0;
+    public static final double VENUS_SPACE_POSZ = 0.0;
+    public static final float VENUS_SPACE_SIZE = 0.5f;
+    public static final double VENUS_HEIGHT = 128.0;
     // EARTH
     public static final double OVERWORLD_SPACE_POSX = 0.0;
-    public static final double OVERWORLD_SPACE_POSY = 0.0;
+    public static final double OVERWORLD_SPACE_POSY = 12.0;
     public static final double OVERWORLD_SPACE_POSZ = 0.0;
-    public static final float OVERWORLD_SPACE_SIZE = 1.0f;
+    public static final float OVERWORLD_SPACE_SIZE = 0.5f;
     public static final double OVERWORLD_HEIGHT = 128.0;
 
-    // MOON (NYI, only renders for now)
+    // MOON
     public static final double MOON_SPACE_POSX = 0.0;
-    public static final double MOON_SPACE_POSY = 6.0;
+    public static final double MOON_SPACE_POSY = 18.0;
     public static final double MOON_SPACE_POSZ = 0.0;
     public static final float MOON_SPACE_SIZE = 0.5f;
     public static final double MOON_HEIGHT = 128.0;
 
+    // Mars
+    public static final double MARS_SPACE_POSX = 0.0;
+    public static final double MARS_SPACE_POSY = 24.0;
+    public static final double MARS_SPACE_POSZ = 0.0;
+    public static final float MARS_SPACE_SIZE = 0.5f;
+    public static final double MARS_HEIGHT = 128.0;
+
+
+
     // Misc config
     public static final double PLANET_3D_RENDER_DIST = 1000.0; // Distance at which planets go from 3D to 2D
     // Actually creating the planets
-    public static Planet EARTH = new Planet(OVERWORLD_SPACE_SIZE, OVERWORLD_SPACE_POSX, OVERWORLD_SPACE_POSY, OVERWORLD_SPACE_POSZ, 16.0f, -20.0f, new Identifier("propulsive:textures/planets/earth.png"),  new Identifier("propulsive:textures/planets/earth_icon.png"));
-    public static Planet MOON = new Planet(MOON_SPACE_SIZE, MOON_SPACE_POSX, MOON_SPACE_POSY, MOON_SPACE_POSZ, 45.0f, 25.0f, new Identifier("propulsive:textures/planets/moon.png"),  new Identifier("propulsive:textures/planets/moon_icon.png"));
+
+    public static Planet MERCURY = new Planet(
+            MERCURY_SPACE_SIZE,
+            MERCURY_SPACE_POSX,
+            MERCURY_SPACE_POSY,
+            MERCURY_SPACE_POSZ,
+            0.0f,
+            0.0f,
+            new Identifier("propulsive:textures/planets/mercury.png"),
+            new Identifier("propulsive:textures/planets/mercury_icon.png")
+    );
+
+    public static Planet VENUS = new Planet(
+            VENUS_SPACE_SIZE,
+            VENUS_SPACE_POSX,
+            VENUS_SPACE_POSY,
+            VENUS_SPACE_POSZ,
+            0.0f,
+            0.0f,
+            new Identifier("propulsive:textures/planets/venus.png"),
+            new Identifier("propulsive:textures/planets/venus_icon.png")
+    );
+    public static Planet EARTH = new Planet(
+            OVERWORLD_SPACE_SIZE,
+            OVERWORLD_SPACE_POSX,
+            OVERWORLD_SPACE_POSY,
+            OVERWORLD_SPACE_POSZ,
+            0.0f,
+            0.0f,
+            new Identifier("propulsive:textures/planets/earth.png"),
+            new Identifier("propulsive:textures/planets/earth_icon.png")
+    );
+    public static Planet MOON = new Planet(
+            MOON_SPACE_SIZE,
+            MOON_SPACE_POSX,
+            MOON_SPACE_POSY,
+            MOON_SPACE_POSZ,
+            0.0f,
+            0.0f,
+            new Identifier("propulsive:textures/planets/moon.png"),
+            new Identifier("propulsive:textures/planets/moon_icon.png")
+    );
+    public static Planet MARS = new Planet(
+            MARS_SPACE_SIZE,
+            MARS_SPACE_POSX,
+            MARS_SPACE_POSY,
+            MARS_SPACE_POSZ,
+            0.0f,
+            0.0f,
+            new Identifier("propulsive:textures/planets/mars.png"),
+            new Identifier("propulsive:textures/planets/mars_icon.png")
+    );
 
     public static Identifier id(String path) {
         return new Identifier("propulsive", path);
