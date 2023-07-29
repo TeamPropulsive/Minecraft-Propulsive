a0d3c39325ad1a80ea87b4fb7cdab250f22f3142
JuneDeveloper <JuneDeveloper@github.com>
2023-07-29 14:53:38 +0100

    Allowed for the use of dimension-specific worldborders

--- src/main/java/com/june/propulsive/celestial/Terrestrial.java
+++ src/main/java/com/june/propulsive/celestial/Terrestrial.java
@@ -13,17 +13,9 @@ public class Terrestrial extends Planet {
     public Terrestrial(double scale, double posX, double posY, double posZ, float horizontalRotation, float verticalRotation, Identifier texture2d, Identifier texture3d) {
         super(scale, posX, posY, posZ, horizontalRotation, verticalRotation, texture2d, texture3d);
     }
-    @Override
-    public void tick(MinecraftServer server) {
-        List<ServerPlayerEntity> players = server.getPlayerManager().getPlayerList();
-        for (ServerPlayerEntity player : players) {
-            if (player.getWorld().getRegistryKey() == SPACE) {
-                double distance = player.getPos().subtract(this.planetPos).length();
-                if (distance < (this.planetSize * 2.01)) {
-                    //DimensionHandler.TeleportDimension(player, OVERWORLD, 0, 100, 0);
-                }
-            }
 
-        }
+    @Override
+    public void collisionDetected(ServerPlayerEntity player) {
+
     }
 }
