ab2dd38af1778d6d2f3494cfbbde47610090bdd9
Juniper <86971433+JuneDeveloper@users.noreply.github.com>
2023-08-05 15:16:37 +0100

    Update GravityHandler.java

--- src/main/java/com/june/propulsive/handler/GravityHandler.java
+++ src/main/java/com/june/propulsive/handler/GravityHandler.java
@@ -6,8 +6,10 @@ import static com.june.propulsive.Propulsive.SPACE;
 
 public class GravityHandler {
     public static float currentGravity(Entity entity) {
-        if (entity.getWorld().getRegistryKey() == SPACE)
+        if (entity.getWorld().getRegistryKey() == SPACE && !entity.isInSneakingPose())
             return 0f;
+        else if (entity.getWorld().getRegistryKey() == SPACE && entity.isInSneakingPose())
+            return 1f;
         return 1f;
     }
 }
