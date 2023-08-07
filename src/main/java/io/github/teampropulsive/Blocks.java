07ea81b07ef3b3e27045de4687b0623f12695f98
Juniper <86971433+JuneDeveloper@users.noreply.github.com>
2023-08-07 19:46:01 +0100

    Added anorthosite

--- src/main/java/io/github/teampropulsive/Blocks.java
+++ src/main/java/io/github/teampropulsive/Blocks.java
@@ -6,16 +6,19 @@ import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 
 public class Blocks {
-    public static final Block MOON_REGOLITH = new Block( // Surface block
+    public static final Block MOON_REGOLITH = new Block(
            FabricBlockSettings.create().requiresTool().strength(3.0f, 9.0f)
     );
-    public static final Block VOLCANIC_MOON_REGOLITH = new Block( // Surface block for select biomes
+    public static final Block VOLCANIC_MOON_REGOLITH = new Block(
             FabricBlockSettings.create().requiresTool().strength(4.0f, 10.0f)
     );
-
+    public static final Block ANORTHOSITE = new Block(
+            FabricBlockSettings.create().requiresTool().strength(5.0f, 11.0f)
+    );
     public static void register() {
         registerBlock(MOON_REGOLITH, "lunar_regolith");
         registerBlock(VOLCANIC_MOON_REGOLITH, "volcanic_lunar_regolith");
+        registerBlock(ANORTHOSITE, "anorthosite");
     }
 
     private static void registerBlock(Block block, String name) {
