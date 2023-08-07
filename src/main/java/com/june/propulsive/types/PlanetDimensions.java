72aacc8f744b93c9c3bac3ea26fa0556aeb88e7c
arthomnix <arthomnix@arthomnix.dev>
2023-08-07 17:49:53 +0100

    this is literally what records were invented for

--- src/main/java/com/june/propulsive/types/PlanetDimensions.java
+++ src/main/java/com/june/propulsive/types/PlanetDimensions.java
@@ -3,21 +3,11 @@ package com.june.propulsive.types;
 import net.minecraft.registry.RegistryKey;
 import net.minecraft.world.World;
 
-public class PlanetDimensions {
-    public RegistryKey<World> top;
-    public RegistryKey<World> bottom;
-    public RegistryKey<World> left;
-    public RegistryKey<World> right;
-    public RegistryKey<World> front;
-    public RegistryKey<World> back;
-
-    public PlanetDimensions(RegistryKey<World> top, RegistryKey<World> bottom, RegistryKey<World> left, RegistryKey<World> right, RegistryKey<World> back, RegistryKey<World> front) {
-        this.top = top;
-        this.bottom = bottom;
-        this.left = left;
-        this.right = right;
-        this.front = front;
-        this.back = back;
-    }
-
-}
+public record PlanetDimensions(
+        RegistryKey<World> top,
+        RegistryKey<World> bottom,
+        RegistryKey<World> left,
+        RegistryKey<World> right,
+        RegistryKey<World> front,
+        RegistryKey<World> back
+) {}
