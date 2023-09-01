0966cda3be80e3f26e9bf9bd96a28e9a0604a4fc
Juniper <86971433+JuneDeveloper@users.noreply.github.com>
2023-09-01 17:17:50 +0100

    Added aluminum loot tables and made require iron pickaxe

--- src/main/java/io/github/teampropulsive/Blocks.java
+++ src/main/java/io/github/teampropulsive/Blocks.java
@@ -13,13 +13,13 @@ public class Blocks {
             FabricBlockSettings.create().requiresTool().strength(4.0f, 10.0f)
     );
     public static final Block ALUMINUM_DEEPSLATE_ORE = new Block(
-            FabricBlockSettings.create().requiresTool().strength(4.5f, 3.0f)
+            FabricBlockSettings.create().requiresTool().strength(4.5f, 3.0f).requiresTool()
     );
     public static final Block ALUMINUM_ORE = new Block(
-            FabricBlockSettings.create().requiresTool().strength(1.5f, 6.0f)
+            FabricBlockSettings.create().requiresTool().strength(1.5f, 6.0f).requiresTool()
     );
     public static final Block ALUMINUM_BLOCK = new Block(
-            FabricBlockSettings.create().requiresTool().strength(5.0f, 6.0f)
+            FabricBlockSettings.create().requiresTool().strength(5.0f, 6.0f).requiresTool()
     );
     public static final Block ANORTHOSITE = new Block(
             FabricBlockSettings.create().requiresTool().strength(5.0f, 11.0f)
