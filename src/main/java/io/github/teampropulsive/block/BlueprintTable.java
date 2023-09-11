7bee0436934be23c8cb54dc5b36af15c6ea08408
JustAPotota <justapotota.dev@gmail.com>
2023-09-11 15:25:31 -0500

    Use block model for blueprint table like a normal mod dev

--- src/main/java/io/github/teampropulsive/block/BlueprintTable.java
+++ src/main/java/io/github/teampropulsive/block/BlueprintTable.java
@@ -1,28 +1,20 @@
 package io.github.teampropulsive.block;
 
-import net.fabricmc.api.EnvType;
-import net.fabricmc.api.Environment;
+import net.minecraft.block.BlockRenderType;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.BlockWithEntity;
 import net.minecraft.block.entity.BlockEntity;
-import net.minecraft.client.MinecraftClient;
-import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.PlayerInventory;
-import net.minecraft.item.ItemStack;
-import net.minecraft.screen.*;
-import net.minecraft.stat.Stats;
+import net.minecraft.screen.NamedScreenHandlerFactory;
+import net.minecraft.screen.ScreenHandler;
 import net.minecraft.text.Text;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.Vec3i;
 import net.minecraft.world.World;
 import org.jetbrains.annotations.Nullable;
-
-import static io.github.teampropulsive.block.Blocks.LAUNCH_PAD;
-import static io.github.teampropulsive.block.Blocks.LAUNCH_TOWER;
 
 public class BlueprintTable extends BlockWithEntity implements NamedScreenHandlerFactory  {
 
@@ -30,7 +22,10 @@ public class BlueprintTable extends BlockWithEntity implements NamedScreenHandle
         super(settings);
     }
 
-
+    @Override
+    public BlockRenderType getRenderType(BlockState state) {
+        return BlockRenderType.MODEL;
+    }
 
     @Override
     public ActionResult onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit) {
