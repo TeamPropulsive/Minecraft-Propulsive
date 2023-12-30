d0e152a913e9c38c29307706916332d52ba372b9
Juniper Primavera <juniper.gardiner@gmail.com>
2023-12-30 14:00:19 +0000

    Rewrite wahoo

---
+++ src/main/java/io/github/teampropulsive/propulsive/Propulsive.java
@@ -0,0 +1,10 @@
+package io.github.teampropulsive.propulsive;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Propulsive implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
