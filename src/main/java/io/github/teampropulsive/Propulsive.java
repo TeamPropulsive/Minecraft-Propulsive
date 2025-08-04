f771b8cbc2178ab909457008eda3b7e8c02a8277
Juniper Gardiner <86971433+JunePrimavera@users.noreply.github.com>
2025-08-04 23:17:19 +0100

    It compiles

--- src/main/java/io/github/teampropulsive/Propulsive.java
+++ src/main/java/io/github/teampropulsive/Propulsive.java
@@ -1,23 +1,20 @@
 package io.github.teampropulsive;
 
-import io.github.teampropulsive.celestial.Star;
-import io.github.teampropulsive.celestial.Terrestrial;
-import io.github.teampropulsive.types.Planet;
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.loader.api.FabricLoader;
-import net.minecraft.entity.damage.DamageType;
 import net.minecraft.registry.RegistryKey;
 import net.minecraft.registry.RegistryKeys;
 import net.minecraft.util.Identifier;
 import net.minecraft.world.World;
 
-import java.util.ArrayList;
-
 
 public class Propulsive implements ModInitializer {
 
+    public static RegistryKey<World> SPACE = RegistryKey.of(RegistryKeys.WORLD, new Identifier("propulsive:space"));
+
     @Override
     public void onInitialize() {
+
+    }
 
     public static Identifier id(String path) {
         return new Identifier("propulsive", path);
--- src/main/java/io/github/teampropulsive/PropulsiveClient.java
+++ src/main/java/io/github/teampropulsive/PropulsiveClient.java
@@ -9,15 +9,6 @@ import net.fabricmc.fabric.api.client.rendering.v1.DimensionRenderingRegistry;
 import net.minecraft.registry.RegistryKey;
 import net.minecraft.registry.RegistryKeys;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.URL;
-import java.util.ArrayList;
-import java.util.List;
-
-import static io.github.teampropulsive.Propulsive.*;
-import static io.github.teampropulsive.keybind.MapScreenKeybind.MapScreenKeybindRegister;
 
 @Environment(EnvType.CLIENT)
 public class PropulsiveClient implements ClientModInitializer {
