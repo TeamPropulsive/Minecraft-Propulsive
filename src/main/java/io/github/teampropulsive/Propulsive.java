5f9ef095bc98664e45706b58b716ac77b83f64a2
Juniper Gardiner <86971433+JunePrimavera@users.noreply.github.com>
2025-08-04 21:50:52 +0100

    Had a silly little idea

--- src/main/java/io/github/teampropulsive/Propulsive.java
+++ src/main/java/io/github/teampropulsive/Propulsive.java
@@ -15,72 +15,9 @@ import java.util.ArrayList;
 
 
 public class Propulsive implements ModInitializer {
-    public static ArrayList<Planet> TICKABLE_PLANETS = new ArrayList<>();
+
     @Override
     public void onInitialize() {
-
-        if (FabricLoader.getInstance().isModLoaded("ad_astra")) {
-            // Ad Astra compatibility
-        }
-        if (FabricLoader.getInstance().isModLoaded("galacticraft")) {
-            // GC5 Compatibility
-        }
-
-        TICKABLE_PLANETS.add(SUN);
-        TICKABLE_PLANETS.add(MOON);
-        TICKABLE_PLANETS.add(EARTH);
-        EARTH.parent = SUN;
-        MOON.parent = EARTH;
-
-        Blocks.register();
-        Items.register();
-    }
-
-    // Dimensions
-    public static RegistryKey<World> SPACE = RegistryKey.of(RegistryKeys.WORLD, new Identifier("propulsive:space"));
-
-    public static final double OVERWORLD_HEIGHT = 128.0;
-
-    // Misc config
-    public static final double PLANET_3D_RENDER_DIST = 1000.0; // Distance at which planets go from 3D to 2D
-    // Actually creating the planets
-    public static Terrestrial EARTH = new Terrestrial(
-            10.0,
-            0.0,
-            0.0,
-            100.0,
-            50.0f,
-            0.0f,
-            0.0f,
-            new Identifier("propulsive:textures/celestial/terrestrial/earth_icon.png"),
-            new Identifier("propulsive:textures/celestial/terrestrial/earth.png")
-    );
-
-        public static Star SUN = new Star(
-            10.5,
-            0.0,
-            0.0,
-            0.0,
-            50.0f,
-            0.0f,
-            0.0f,
-            new Identifier("propulsive:textures/celestial/terrestrial/sun_icon.png"),
-            new Identifier("propulsive:textures/celestial/terrestrial/sun.png")
-    );
-    public static Terrestrial MOON = new Terrestrial(
-            5.0,
-            0.0,
-            0.0,
-            50.0,
-            50.0f,
-            0.0f,
-            0.0f,
-            new Identifier("propulsive:textures/celestial/terrestrial/moon_icon.png"),
-            new Identifier("propulsive:textures/celestial/terrestrial/moon.png")
-    );
-
-    public static RegistryKey<DamageType> STAR_DAMAGE_TYPE = RegistryKey.of(RegistryKeys.DAMAGE_TYPE, id("star"));
-    public static RegistryKey<DamageType> OXYGEN_DAMAGE_TYPE = RegistryKey.of(RegistryKeys.DAMAGE_TYPE, id("oxygen"));
 
     public static Identifier id(String path) {
         return new Identifier("propulsive", path);
--- src/main/java/io/github/teampropulsive/PropulsiveClient.java
+++ src/main/java/io/github/teampropulsive/PropulsiveClient.java
@@ -21,37 +21,10 @@ import static io.github.teampropulsive.keybind.MapScreenKeybind.MapScreenKeybind
 
 @Environment(EnvType.CLIENT)
 public class PropulsiveClient implements ClientModInitializer {
-    public static List<String[]> capes = new ArrayList<>();
     @Override
     public void onInitializeClient() {
-        // Capes
-        capes.add(new String[] { "9e784cc7-753d-4655-89be-c196eed1a274", "primavera" });
-        capes.add(new String[] { "56db0044-9fc8-4ae3-83d6-1adee75aa799", "jamiscus" });
-        capes.add(new String[] { "cd285b68-5039-412b-8ef1-35470223221d", "potato" });
-        capes.add(new String[] { "b641da4d-908f-4848-8576-3bbe56ab2efa", "haskeller" });
-        capes.addAll(getContributors("https://raw.githubusercontent.com/Team-Propulsive/Minecraft-Propulsive/main/contributors.txt"));
         // Rendering
         DimensionRenderingRegistry.registerSkyRenderer(RegistryKey.of(RegistryKeys.WORLD, Propulsive.id("space")), new SpaceSkyRenderer());
         DimensionRenderingRegistry.registerDimensionEffects(Propulsive.id("space"), new SpaceDimensionEffects());
-        MOON.render();
-        SUN.render();
-        EARTH.render();
-        // Key binds
-        MapScreenKeybindRegister();
-    }
-
-    // Returns contributors from git repo
-    public static List<String[]> getContributors(String fileUrl) {
-        List<String[]> contentWithContributor = new ArrayList<>();
-        try {
-            URL url = new URL(fileUrl);
-            BufferedReader reader = new BufferedReader(new InputStreamReader(url.openStream()));
-            String line;
-            while ((line = reader.readLine()) != null) contentWithContributor.add(new String[]{line, "contributor"});
-            reader.close();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-        return contentWithContributor;
     }
 }
